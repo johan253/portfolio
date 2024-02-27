56c836eed17fdb4b3dce96cf52222a4ccd5eff76
johan253 <johannjo2000@gmail.com>
2024-02-27 15:40:31 -0800

    added animations and changed coloring

--- src/app/Project.js
+++ src/app/Project.js
@@ -3,19 +3,19 @@ import {AiFillGithub} from "react-icons/ai";
 
 export default function Project(props) {
     const listTags = props.tags.map(tag =>
-        <div key={tag.name} className={"bg-gradient-to-br from-sky-500 to-sky-400 rounded-2xl py-1 px-3 inline-block m-2 shadow-xl"}>{tag.name}</div>)
+        <div key={tag.name} className={"bg-sky-500 rounded-2xl py-1 px-3 inline-block m-2 text-white"}>{tag.name}</div>)
 return (
-    <div className={"bg-gradient-to-br from-blue-900 to-blue-700 max-w-screen-xl overflow-hidden rounded-2xl my-12 mx-auto shadow-lg shadow-slate-900"}>
-        <div className={"md:flex"}>
+    <div className={"bg-gradient-to-br from-neutral-100 dark:from-slate-900 dark:to-slate-800 max-w-screen-xl overflow-hidden rounded-2xl my-12 mx-auto shadow-md"}>
+        <div className={"md:flex group"}>
             <div className={"md:shrink-0 p-2"}>
                 <Image src={props.img} alt={props.title}
                 className={"rounded-2xl h-48 w-full object-cover md:h-full md:w-56"}/>
             </div>
-            <div className={"flex-wrap p-4 md:inline-block"}>
-                <h1 className={"flex text-2xl font-mono justify-between"}>
+            <div className={"flex-wrap p-4 md:inline-block text-black dark:text-white"}>
+                <h1 className={"flex text-2xl font-mono justify-between transition ease-in group-hover:text-sky-500 font-bold"}>
                     {props.title}
                     <a href={props.url} target={"_blank"} rel={"noopener noreferrer"}>
-                        <AiFillGithub className={"scale-150"}/>
+                        <AiFillGithub className={"scale-125 transition hover:scale-150"}/>
                     </a>
                 </h1>
                 <p className={"text-md font-mono p-4"}>{props.desc}</p>
--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ import johan from "../assets/johan.png";
 import Project from "@/app/Project";
 import projection from "../assets/projection.png";
 import tetris from "../assets/tetris.png";
+import portfolio from "../assets/portfolio.png";
 import {useState} from "react";
-
 
 export default function Home() {
     const projectionTags = [
@@ -21,13 +21,18 @@ export default function Home() {
         {name: "MVC"},
         {name: "Observer Design Pattern"}
     ]
-    const [darkMode, setDarkMode] = useState(false);
+    const portfolioTags = [
+        {name: "React"},
+        {name: "TailwindCSS"},
+        {name: "Next.js"},
+    ]
+    const [darkMode, setDarkMode] = useState(true);
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
     }
   return (
     <main className={darkMode ? "dark" : ""}>
-      <section className="bg-neutral-200 dark:bg-slate-800">
+      <section className="transition bg-neutral-200 dark:bg-slate-800">
           <nav className="mb-10 flex justify-between p-5 w-full">
               <div className={"bg-neutral-50 shadow-md shadow-neutral-300 p-3 text-white rounded-md dark:bg-slate-700 dark:shadow-slate-950"}>
                   <h1 className={"bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-transparent font-bold"}>
@@ -35,10 +40,10 @@ export default function Home() {
                   </h1>
                 </div>
               <ul className="flex items-center">
-                  <li>
+                  <li className={"transition ease-in-out hover:rotate-12 "}>
                       <BsFillMoonStarsFill className="fill-black dark:fill-white cursor-pointer" onClick={()=>toggleDarkMode()}/>
                   </li>
-                  <li>
+                  <li className={"transition ease-in-out hover:scale-95"}>
                       <a href="#" className="bg-gradient-to-br from-blue-900 to-blue-600 text-white px-5 py-2 rounded-md ml-8">
                           Resume
                       </a>
@@ -49,28 +54,28 @@ export default function Home() {
               <p className="font-mono text-black dark:text-white">Hi, my name is</p>
               <h2 className="text-5xl bg-gradient-to-b from-blue-700 to-blue-400 bg-clip-text text-transparent p-2 font-bold">Johan Hernandez</h2>
               <h3 className="text-2xl p-2 text-black dark:text-white">Aspiring Software Engineer</h3>
-              <p className="text-md p-5 leading-8 text-gray-600 dark:text-gray-400">
-                  Computer Science Student at UWT. Passionate about programming and solving problems.
+              <p className="text-md p-5 leading-8 text-gray-600 dark:text-gray-400 ">
+                  Computer Science student at the University of Washington. Passionate about programming and solving problems.
                   Connect with me and get to know me better!
               </p>
           </div>
           <div className="text-3xl flex justify-center gap-14 py-3 text-gray-700 dark:text-gray-400">
               <a href={"https://www.linkedin.com/in/johan253/"} target={"_blank"} rel={"noopener noreferrer"}>
-                  <AiFillLinkedin/>
+                  <AiFillLinkedin className={"transition hover:scale-105 hover:fill-blue-600"}/>
               </a>
               <a href={"https://github.com/johan253"} target={"_blank"} rel={"noopener noreferrer"}>
-                  <AiFillGithub/>
+                  <AiFillGithub className={"transition hover:scale-105 hover:fill-black"}/>
               </a>
               <a href={"mailto:johannjo2000@gmail.com"}>
-                  <AiFillMail/>
+                  <AiFillMail className={"transition hover:scale-105 hover:fill-red-600"}/>
               </a>
           </div>
-          <div className={"relative mx-auto bg-gradient-to-b from-blue-900 rounded-full w-80 h-80 mt-14"}>
+          <div className={"relative mx-auto bg-gradient-to-b from-blue-900 rounded-full w-80 h-80 mt-4"}>
             <Image className={"scale-75 -translate-y-10 -translate-x-1.5"} src={johan} alt={"Animated Image of Johan"}/>
           </div>
       </section>
-        <section className={"p-5 bg-neutral-200 dark:bg-slate-800"}>
-            <h1 className={"text-3xl text-center text-black dark:text-white font-bold"}>
+        <section className={"p-5 bg-neutral-200 dark:bg-slate-800 "}>
+            <h1 className={"text-3xl text-left text-black dark:text-white font-bold"}>
                 Projects
             </h1>
             <Project img={projection} title={"PROJECTion"} url={"https://github.com/johan253/projection"}
@@ -83,6 +88,11 @@ export default function Home() {
                          "Collaborated with class to meet deadlines and create a MVC structure to allow communication with " +
                          "front end and back end."}
                      tags={tetrisTags}/>
+            <Project img={portfolio} title={"Portfolio"} url={"https://github.com/johan253/portfolio"}
+                     desc={"The website that you are currently on was built using Next.js and React, with " +
+                         "TailwindCSS for styling. Simple yet beautiful and responsive UI to fully capture " +
+                         "your attention and introduce myself effectively."}
+                     tags={portfolioTags}/>
         </section>
     </main>
   );
