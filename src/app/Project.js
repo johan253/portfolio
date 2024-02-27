cc5d21a17ec7dabcf524c6776216c7fcc406911c
johan253 <johannjo2000@gmail.com>
2024-02-27 13:48:30 -0800

    fixed Project component Witdh

--- src/app/Project.js
+++ src/app/Project.js
@@ -5,7 +5,7 @@ export default function Project(props) {
     const listTags = props.tags.map(tag =>
         <div key={tag.name} className={"bg-gradient-to-br from-sky-500 to-sky-400 rounded-2xl py-1 px-3 inline-block m-2 shadow-xl"}>{tag.name}</div>)
 return (
-    <div className={"bg-gradient-to-br from-blue-900 to-blue-700 max-w-screen-2xl overflow-hidden rounded-2xl my-4 shadow-lg shadow-slate-900"}>
+    <div className={"bg-gradient-to-br from-blue-900 to-blue-700 max-w-screen-xl overflow-hidden rounded-2xl my-12 mx-auto shadow-lg shadow-slate-900"}>
         <div className={"md:flex"}>
             <div className={"md:shrink-0 p-2"}>
                 <Image src={props.img} alt={props.title}
