a4f7e63010a773908d04879c28f83d5117aff1f9
johan253 <johannjo2000@gmail.com>
2024-08-25 13:40:38 -0700

    Fixed api routes to proper Next.js app router

--- src/app/api/images/johan/route.ts
+++ src/app/api/images/johan/route.ts
@@ -1,5 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ data: 'This is a response!'});
+export async function GET(req: Request) {
+    return Response.json({ data: 'This is a response!'});
 }
