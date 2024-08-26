624e2f7535bc2cfb1fd9ad0b5b5104e0cd76da74
johan253 <johannjo2000@gmail.com>
2024-08-26 16:09:41 -0700

    Added temporary postgres database for workflow and seed database with test data to test app functionality

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+const PrismaClient = require("@prisma/client").PrismaClient;
+const pc = new PrismaClient();
+async function main() {
+  await pc.project.createMany({
+    data: [
+      {
+        name: "Project 1",
+        description: "Description 1",
+        url: "https://github.com",
+        img: "https://via.placeholder.com/150",
+        tags: ["tag1", "tag2"],
+        order: 1,
+      },
+      {
+        name: "Project 2",
+        description: "Description 2",
+        url: "https://github.com",
+        img: "https://via.placeholder.com/150",
+        tags: ["tag1", "tag2"],
+        order: 2,
+      },
+    ],
+  });
+
+  await pc.images.createMany({
+    data: [
+      {
+        name: "johan",
+        url: "https://via.placeholder.com/150",
+      },
+      {
+        name: "resume",
+        url: "https://via.placeholder.com/150",
+      }
+    ]
+  });
+}
+
+main()
+  .then(async () => {
+    await pc.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await pc.$disconnect();
+    process.exit(1);
+  });
