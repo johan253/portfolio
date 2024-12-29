11fe4cd4f71307bb6fdbce19c87cf3c75c116e1b
johan253 <johannjo2000@gmail.com>
2024-12-28 16:13:39 -0800

    reverted to using Nextjs API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isDev = process.env.NODE_ENV === "development";
-
 const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: 
-          isDev
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/"
-      },
-      {
-        source: "/docs",
-        destination: 
-          isDev
-            ? "http://127.0.0.1:8000/api/docs"
-            : "/api/docs"
-      },
-      {
-        source: "/openapi.json",
-        destination: 
-          isDev
-            ? "http://127.0.0.1:8000/api/openapi.json"
-            : "/api/openapi.json"
-      }
-    ];
-  }
 };
 
 export default nextConfig;
---
+++ src/app/api/images/resume/route.ts
@@ -0,0 +1,15 @@
+import prisma from "@/lib/prisma";
+
+const name = "resume";
+
+export const revalidate = 600;
+
+export async function GET() {
+  const imageObj = await prisma.images.findUnique({
+    where: {
+      name
+    }
+  });
+  const imageURL = imageObj.url;
+  return Response.json(imageURL, { status: 200 });
+}
