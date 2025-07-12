ce0451d92366cbf4c35d2bdbb64fb4ac997c5296
johan253 <johannjo2000@gmail.com>
2025-07-11 20:26:47 -0700

    chore: remove react linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,11 +17,4 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  {
-    ...pluginReact.configs.flat.recommended,
-    rules: {
-      ...pluginReact.configs.flat.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-    },
-  },
 ]);
