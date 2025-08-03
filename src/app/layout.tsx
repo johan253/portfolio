a5566107e3c60103d200935a8c3a1d5c5b3e2908
johan253 <johannjo2000@gmail.com>
2025-08-02 19:48:42 -0700

    feat: add vercel analytics and insights
    i know i know, you want me to containerize my application and deploy to
    a $23,419.43 / month Kubernetes cluster... However... It's just a
    portfolio bro... I want it ez

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { logVisit } from "@/lib/actions";
 import React from "react";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   logVisit();
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-900`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-900`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
